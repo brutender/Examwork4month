bd2c6806381a61fe38bff04f206374186581d162
brutender <fakedere@gmail.com>
2022-08-05 21:17:03 +0600

    o

---
+++ src/src/examwork4month/Connection.java
@@ -0,0 +1,22 @@
+package examwork4month;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection {
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String user = "postgres";
+    private final String password = "km-jm-kam-am1";
+
+    public java.sql.Connection connect() {
+        java.sql.Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected succesfully.");
+            System.out.println("Connection closed");
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+        }
+        return connection;
+    }
+}
---
+++ src/src/examwork4month/Main.java
@@ -0,0 +1,8 @@
+package examwork4month;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = new Connection();
+        connection.connect();
+    }
+}
